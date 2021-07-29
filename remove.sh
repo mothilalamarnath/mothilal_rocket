c49aab4e8c0f893fad1d9cc7bda28e9e20b9cb1d
mothilalamarnath <83276912+mothilalamarnath@users.noreply.github.com>
2021-07-29 16:06:47 +0530

    Update remove.sh

--- remove.sh
+++ remove.sh
@@ -1,4 +1,7 @@
 #! /bin/sh
+# To kill all the running containers
 docker kill $(docker ps -q)
+# To clean all the stopped containers
 docker rm $(docker ps -a -q)
+# To remove all the container images
 docker rmi $(docker image ls -q)
